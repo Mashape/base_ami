60e9a8ec75ed8b6675ac82ee46b9489178c1b1eb
Packer.io <colin@hutchic.com>
2016-07-22 13:09:44 -0400

    removed left over references to id_rsa

--- install.sh
+++ install.sh
@@ -7,10 +7,3 @@ build-essential libssl-dev libffi-dev python-dev libcurl4-openssl-dev
 
 sudo pip install https://pypi.python.org/packages/3.4/s/setuptools/setuptools-11.3.1-py2.py3-none-any.whl
 sudo pip install -r /home/ubuntu/requirements.txt
-
-git clone https://github.com/fsaintjacques/semver-tool.git
-cd semver-tool
-chmod 500 /home/ubuntu/.ssh/id_rsa
-ssh-add /home/ubuntu/.ssh/id_rsa
-sudo make install
-rm /home/ubuntu/.ssh/id_rsa
