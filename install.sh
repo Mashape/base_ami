d37dbfd41943e7931aa82b159d135e19cc206ca0
Packer.io <colin@hutchic.com>
2016-07-22 11:22:14 -0400

    created AMI 0.1.8

--- install.sh
+++ install.sh
@@ -5,10 +5,9 @@ sudo apt-get update
 sudo apt-get install -yf python-virtualenv python-distutils-extra python-apt make git \
 build-essential libssl-dev libffi-dev python-dev libcurl4-openssl-dev
 
+sudo pip install https://pypi.python.org/packages/3.4/s/setuptools/setuptools-11.3.1-py2.py3-none-any.whl
+sudo pip install -r requirements.txt
+
 git clone https://github.com/fsaintjacques/semver-tool.git
 cd semver-tool
 sudo make install
-cd ~/
-
-sudo pip install https://pypi.python.org/packages/3.4/s/setuptools/setuptools-11.3.1-py2.py3-none-any.whl
-sudo pip install -r requirements.txt
